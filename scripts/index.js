9a54054d274aafa215fdd8b80a3e0a0907ba06a8
ravijhha <ravijha.ten@gmail.com>
2025-02-04 14:15:22 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 14, 2024',
+  birthdate: 'Feb 14, 2025',
   name: 'Jhadu ji <3'
 };
 
